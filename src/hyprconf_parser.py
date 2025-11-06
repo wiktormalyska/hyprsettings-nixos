08af283782bb817dd8d558e3cd1ef4129351355b
acropolis914 <acropolis914@hed-phsuarnaba@smu.edu.ph>
2025-11-06 22:27:59 +0800

    refactor: Simplify group element creation by introducing ConfiGroup class

--- src/hyprconf_parser.py
+++ src/hyprconf_parser.py
@@ -19,91 +19,87 @@ file_path = f"{Path(__file__).resolve().parent}/ConfigDescriptions.txt"
 
 
 def get_parts(line: str):
-      return line.split("=", 1)[1].strip().strip(",").strip('"')
+	return line.split("=", 1)[1].strip().strip(",").strip('"')
 
 
 class Node:
-      def __init__(self, path, name, type_, data, description):
-            self.name = name
-            self.path = path
-            self.type = type_
-            self.data = data
-            self.description = description
+	def __init__(self, path, name, type_, data, description):
+		self.name = name
+		self.path = path
+		self.type = type_
+		self.data = data
+		self.description = description
 
-      def print_node(self):
-            pass
+	def print_node(self):
+		pass
 
-      def to_dict(self):
-            dict = {
-                  "name": self.name,
-                  "path": self.path,
-                  "type": self.type,
-                  "data": self.data,
-                  "description": self.description,
-            }
-            return dict
+	def to_dict(self):
+		dict = {
+			"name": self.name,
+			"path": self.path,
+			"type": self.type,
+			"data": self.data,
+			"description": self.description,
+		}
+		return dict
 
-      def to_json(self):
-            return json.dumps(self.to_dict(), indent=4)
+	def to_json(self):
+		return json.dumps(self.to_dict(), indent=4)
 
 
 def get_config_descriptions() -> list:
-      descriptions = []
-      with open(file_path, "r", encoding="UTF-8") as file:
-            lines = file.readlines()
-            #     info = {}
-            name = None
-            path = None
-            type_ = None
-            description = ""
-            data = None
-            is_editing_description = False
-            for line in lines:
-                  line = line.strip()
-                  if line.startswith("SConfigOptionDescription"):
-                        pass
-                  elif line.startswith(".value"):
-                        parts = get_parts(line).split(":")
-                        path = ":".join(parts[:1])
-                        name = parts[-1]
-                        if len(parts) - 1 > 1:
-                              print(name)
-                        is_editing_description = False
-                  elif line.startswith(".description"):
-                        parts = get_parts(line)
-                        description = parts
-                        is_editing_description = True
-                  elif line.startswith(".type"):
-                        type_ = line.split("=", 1)[1].strip().strip(",")
-                        is_editing_description = False
-                  elif line.startswith(".data"):
-                        value = line.split("=", 1)[1].strip().strip(",")
-                        match = re.search(r"\{(.+)\}", value)
-                        data = (
-                              match.group(1).strip("") if match else None
-                        )  # doesnt work good with split_bias
-                        is_editing_description = False
-                  elif is_editing_description:
-                        # print(line)
-                        description += " " + line.strip().strip(",").strip('"')
-                  elif line.startswith("}"):
-                        if path and name:
-                              configNode = Node(
-                                    path, name, type_, data, description
-                              ).to_dict()
-                              descriptions.append(configNode)
-                              # print(configNode.to_json())
-                        name = None
-                        path = None
-                        type_ = None
-                        description = ""
-                        data = None
-                        description = ""
-                        is_editing_description = False
-                  else:
-                        pass
-                        # print(f"No shit for line {line}")
-      return descriptions
+	descriptions = []
+	with open(file_path, "r", encoding="UTF-8") as file:
+		lines = file.readlines()
+		#     info = {}
+		name = None
+		path = None
+		type_ = None
+		description = ""
+		data = None
+		is_editing_description = False
+		for line in lines:
+			line = line.strip()
+			if line.startswith("SConfigOptionDescription"):
+				pass
+			elif line.startswith(".value"):
+				parts = get_parts(line).split(":")
+				path = ":".join(parts[:1])
+				name = parts[-1]
+				# if len(parts) - 1 > 1:
+				#       print(name)
+				is_editing_description = False
+			elif line.startswith(".description"):
+				parts = get_parts(line)
+				description = parts
+				is_editing_description = True
+			elif line.startswith(".type"):
+				type_ = line.split("=", 1)[1].strip().strip(",")
+				is_editing_description = False
+			elif line.startswith(".data"):
+				value = line.split("=", 1)[1].strip().strip(",")
+				match = re.search(r"\{(.+)\}", value)
+				data = match.group(1).strip("") if match else None  # doesnt work good with split_bias
+				is_editing_description = False
+			elif is_editing_description:
+				# print(line)
+				description += " " + line.strip().strip(",").strip('"')
+			elif line.startswith("}"):
+				if path and name:
+					configNode = Node(path, name, type_, data, description).to_dict()
+					descriptions.append(configNode)
+					# print(configNode.to_json())
+				name = None
+				path = None
+				type_ = None
+				description = ""
+				data = None
+				description = ""
+				is_editing_description = False
+			else:
+				pass
+				# print(f"No shit for line {line}")
+	return descriptions
 
 
 config_list = get_config_descriptions()
--- src/ui/scripts/configRenderer.js
+++ src/ui/scripts/configRenderer.js
@@ -116,23 +116,7 @@ export class configRenderer {
             //
             if (json["position"] && json["position"].split(":").length > 1) {
                 //
-                let group_el = document.createElement("div")
-                group_el.classList.add("config-group")
-                group_el.setAttribute("tabindex", 0)
-                group_el.classList.add("editor-item")
-                group_el.dataset.name = json["name"]
-                group_el.dataset.uuid = json["uuid"]
-                group_el.dataset.postion = json["position"]
-                group_el.setAttribute("title", json["position"].replace("root:", ""))
-                if (json["comment"]) {
-                    group_el.dataset.comment = json["comment"]
-                }
-                group_el.addEventListener("keydown", (e) => {
-                    if (e.key == "Enter") {
-                        group_el.querySelector(".editor-item").focus()
-                        console.log("Group is entered")
-                    }
-                })
+                let group_el = new ConfiGroup(json).return()
                 this.current_container.at(-1).appendChild(group_el)
                 this.current_container.push(group_el)
             }
@@ -726,4 +710,30 @@ class EditorItem_Binds {
 
 }
 
+
+class ConfiGroup {
+    constructor(json) {
+        this.group_el = document.createElement("div")
+        this.group_el.classList.add("config-group")
+        this.group_el.setAttribute("tabindex", 0)
+        this.group_el.classList.add("editor-item")
+        this.group_el.dataset.name = json["name"]
+        this.group_el.dataset.uuid = json["uuid"]
+        this.group_el.dataset.postion = json["position"]
+        this.group_el.setAttribute("title", json["position"].replace("root:", ""))
+        if (json["comment"]) {
+            this.group_el.dataset.comment = json["comment"]
+        }
+        this.group_el.addEventListener("keydown", (e) => {
+            if (e.key == "Enter") {
+                group_el.querySelector(".editor-item").focus()
+                console.log("Group is entered")
+            }
+        })
+    }
+
+    return() {
+        return this.group_el
+    }
+}
 
